795281632a9efa1f308c61192e33d99df7558464
rg3915 <regis42santos@gmail.com>
2024-07-01 04:23:40 -0300

    Aula 03 CRUD: Exercicio: adiciona teste para 404 NOT_FOUND

--- fast_zero/tests/test_app.py
+++ fast_zero/tests/test_app.py
@@ -60,6 +60,19 @@ def test_update_user(client):
     }
 
 
+def test_update_user_return_not_found(client):
+    response = client.put(
+        '/users/2',
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
+        },
+    )
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
+
+
 def test_delete_user(client):
     response = client.delete('/users/1')
 
