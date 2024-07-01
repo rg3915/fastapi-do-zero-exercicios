5da57ab4ab4de49a27f2cbb4f7b34f74539c2117
rg3915 <regis42santos@gmail.com>
2024-07-01 04:41:31 -0300

    Aula 03 CRUD: Exercicio: adiciona endpoint get_user e os testes.

--- fast_zero/tests/test_app.py
+++ fast_zero/tests/test_app.py
@@ -43,6 +43,22 @@ def test_read_users(client):
     }
 
 
+def test_get_user(client):
+    response = client.get('/users/1')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'alice',
+        'email': 'alice@example.com',
+        'id': 1,
+    }
+
+
+def test_get_user_return_not_found(client):
+    response = client.get('/users/2')
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
+
+
 def test_update_user(client):
     response = client.put(
         '/users/1',
