b7f360934ac55164ca3b64d955bf0b9b4041bca8
rg3915 <regis42santos@gmail.com>
2024-07-01 03:04:40 -0300

    Aula 02: Adiciona Message em schemas.py

--- fast_zero/fast_zero/app.py
+++ fast_zero/fast_zero/app.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 
+from fast_zero.schemas import Message
+
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=200, response_model=Message)
 def read_root():
-    return {'message': 'Olá Mundo!'}
+    return {'message': 'Olá mundo'}
